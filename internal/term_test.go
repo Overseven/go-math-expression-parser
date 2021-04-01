faaa6c6a9976e77af87f74efdbd19299792c4268
Overseven <specregister@gmail.com>
2021-04-01 22:40:24 +0300

    finish term_test

--- internal/term_test.go
+++ internal/term_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestGetVarList(t *testing.T) {
-	t1, t2, t3 := "a", "b", "c"
+	t1, t2, t3 := "", "b", "145"
 	term1 := internal.Term{Val: t1}
 	term2 := internal.Term{Val: t2}
 	term3 := internal.Term{Val: t3}
@@ -19,12 +19,8 @@ func TestGetVarList(t *testing.T) {
 	var vars = map[string]interface{}{}
 	term1.GetVarList(vars)
 
-	if len(vars) != 1 {
+	if len(vars) != 0 {
 		t.Error("incorrect map keys count = " + strconv.Itoa(len(vars)))
-	}
-
-	if _, ok := vars[t1]; !ok {
-		t.Error("not found t1")
 	}
 
 	vars = map[string]interface{}{}
@@ -41,26 +37,19 @@ func TestGetVarList(t *testing.T) {
 	vars = map[string]interface{}{}
 	term3.GetVarList(vars)
 
-	if len(vars) != 1 {
+	if len(vars) != 0 {
 		t.Error("incorrect map keys count = " + strconv.Itoa(len(vars)))
 	}
-
-	if _, ok := vars[t3]; !ok {
-		t.Error("not found t3")
-	}
-
 }
 
 func TestEvaluate(t *testing.T) {
 	p := parser.NewParser()
-	//p.AddFunction(foo, "foo")
-	//p.AddFunction(average, "average")
-	//exp1 := "foo(average(2, 4, 9), 100)"
 
 	term1 := internal.Term{Val: ""}
 	term2 := internal.Term{Val: "4"}
 	term3 := internal.Term{Val: "a"}
 	term4 := internal.Term{Val: "var3000"}
+	term5 := internal.Term{Val: "R"}
 
 	var vars = map[string]float64{"a": 17.7, "var3000": 30012}
 	res, err := term1.Evaluate(vars, p)
@@ -75,18 +64,29 @@ func TestEvaluate(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if !almostEqual(res, 105.0) {
+	if !fuzzyEqual(res, 4.0) {
 		t.Error("incorrect result = " + strconv.FormatFloat(res, 'e', 4, 64))
 	}
 
-	res, err = f3.Evaluate(vars, p)
-	if err == nil || res != -16 {
-		t.Error("foo error was not handled!")
+	res, err = term3.Evaluate(vars, p)
+	if err != nil {
+		t.Error(err)
+	}
+	if  !fuzzyEqual(res, 17.7) {
+		t.Error("incorrect result = " + strconv.FormatFloat(res, 'e', 4, 64))
 	}
 
-	res, err = f4.Evaluate(vars, p)
-	if err == nil || res != -1 {
-		t.Error("foo error was not handled!")
+	res, err = term4.Evaluate(vars, p)
+	if err != nil {
+		t.Error(err)
+	}
+	if  res != 30012.0 {
+		t.Error("incorrect result = " + strconv.FormatFloat(res, 'e', 4, 64))
+	}
+
+	_, err = term5.Evaluate(vars, p)
+	if err == nil {
+		t.Error("incorrect error handling")
 	}
 }
 
@@ -95,8 +95,8 @@ func TestString(t *testing.T) {
 	term2 := internal.Term{Val: "4"}
 	term3 := internal.Term{Val: "9"}
 	term4 := internal.Term{Val: "100"}
-	f1 := userfunc.Func{"average", []interfaces.Expression{&term1, &term2, &term3}}
-	f2 := userfunc.Func{"foo", []interfaces.Expression{&f1, &term4}}
+	f1 := userfunc.Func{Op: "average", Args: []interfaces.Expression{&term1, &term2, &term3}}
+	f2 := userfunc.Func{Op: "foo", Args: []interfaces.Expression{&f1, &term4}}
 
 	if f1.String() != "( average ( 2,4,9 ) )" {
 		t.Error("incorrect string conversion = " + f1.String())
